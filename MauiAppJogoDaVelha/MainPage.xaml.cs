a9c0dec1465f5648c800b6bf5395b67bcde96632
Erica Giselle Francisco Luz <erica.nicollas_hbn@yahoo.com>
2024-10-15 21:20:28 -0300

    criação dos botoes e colocando o .vs/ no .gitignore

---
+++ MauiAppJogoDaVelha/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+namespace MauiAppJogoDaVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "X";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (vez == "X")
+            {
+                btn.Text = "X";
+                vez = "O";
+            }else
+            {
+                btn.Text = "O";
+                vez = "X";
+            }
+
+        }
+    }
+
+}
